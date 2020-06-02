bf2bdd5313fed74bc77875b4868950f918c86348
刘腾 <11111040@vivo.com>
2020-06-02 20:13:46 +0800

    add detile

--- src/main/java/skill/BitMap.java
+++ src/main/java/skill/BitMap.java
@@ -1,7 +1,11 @@
+package skill;
 
+/**
+ *  通过 位集合 存储大量数据，减少存储内存
+ */
 public class BitMap {
     private long length;
-    private static int[] bitmap;
+    private int[] bitmap;
     private static final int[] BIT_VALUE = {0x00000001, 0x00000002, 0x00000004, 0x00000008, 0x00000010, 0x00000020,
             0x00000040, 0x00000080, 0x00000100, 0x00000200, 0x00000400, 0x00000800, 0x00001000, 0x00002000, 0x00004000,
             0x00008000, 0x00010000, 0x00020000, 0x00040000, 0x00080000, 0x00100000, 0x00200000, 0x00400000, 0x00800000,
