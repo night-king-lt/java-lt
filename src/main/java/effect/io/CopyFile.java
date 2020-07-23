a2944626bebf6da2033373050727537c67300521
刘腾 <11111040@vivo.com>
2020-07-23 11:48:31 +0800

    add fink sink hdfs gz压缩sink

---
+++ src/main/java/effect/io/CopyFile.java
@@ -0,0 +1,32 @@
+package effect.io;
+
+import java.io.*;
+
+/**
+ * @author liuteng
+ * @version 1.0
+ * @date 2020/7/6
+ */
+public class CopyFile {
+
+    public static void main(String[] args) {
+        String p1 = "src/main/resources/source.mp4";
+        String p2 = "src/main/resources/output.mp4";
+        run(p1, p2);
+    }
+
+    public static void run(String path1, String path2) {
+        try(FileInputStream  in = new FileInputStream(new File(path1));
+            FileOutputStream out = new FileOutputStream(new File(path2))
+        ){
+            byte[] b = new byte[200];
+            int len;
+            while((len = in.read(b)) != -1){
+                out.write(b, 0, len);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+}
