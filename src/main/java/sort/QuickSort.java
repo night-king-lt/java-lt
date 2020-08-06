695466a7624a23baca7f6270527e66fc29d66f06
11111040 <11111040@vivo.com>
2020-08-06 15:24:49 +0800

    add

---
+++ src/main/java/sort/QuickSort.java
@@ -0,0 +1,57 @@
+package sort;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        String s = "2,3,5,11,1064,1425,1791,1792,2054,4061,6172,7308,7309,7426,7463,9351,9762,9949,13767,18964,25187,25194,25196,25203,25204,25205,25733,25734,25778,27323,27961,27962,29805,29806,29807,29808,29809,29810,29811,29812,29813,30179,30346,30543,31504,32942,32943,20,21,22,23,28,1423,1424,1426,6173,9352,13271,13272,13273,13274,13768,18667,18668,18822,18823,18824,29620,29621,29622,29623,29624,29625,29626,29627,29628,32944,40830,40835,52592,40810,43688,43715,43718,51931,43716,29620,29621,29623,29627,56780";
+        String[] strings = s.split(",");
+        int[] result = stringToInt(strings);
+//        for (int next: result){
+//            System.out.print(next + " ");
+//        }
+        qSort(result, 0, result.length - 1);
+        for (int next: result){
+            System.out.print(next + ",");
+        }
+    }
+
+    public static int[] stringToInt(String[] strings){
+        int[] result = new int[strings.length];
+        for (int i=0; i< strings.length; i++ ){
+            try{
+                result[i] = Integer.parseInt(strings[i]);
+            }catch (Exception e){
+                System.out.println(strings[i] + " : " +i );
+            }
+
+        }
+        return result;
+    }
+
+    public static void qSort(int[] n, int low, int hight){
+        if ( low >= hight){
+            return ;
+        }
+        int i, j, t, tmp;
+        i = low ;
+        j = hight;
+        t = n[low];
+        while ( i < j ){
+            while( t <= n[j] && i < j){
+                j--;
+            }
+            while( t >= n[i] && i < j){
+                i++;
+            }
+            if( i < j){
+                tmp = n[j];
+                n[j] = n[i];
+                n[i] = tmp;
+            }
+        }
+        n[low] = n[i];
+        n[i] = t;
+        qSort(n, low, i);
+        qSort(n, i+1, hight);
+
+    }
+}
