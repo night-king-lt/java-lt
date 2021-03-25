6c01d847b1ccc6fc54a01b1e23d56bd4422ff16d
11111040 <teng.liu@vivo.com>
2021-03-25 14:46:31 +0800

    add flink iterations

--- src/main/java/flink/iterations/IterationsTest.java
+++ src/main/java/flink/iterations/IterationsTest.java
@@ -9,9 +9,13 @@ import org.apache.flink.api.java.tuple.Tuple5;
 import org.apache.flink.streaming.api.collector.selector.OutputSelector;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.IterativeStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.datastream.SplitStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import org.apache.flink.util.Collector;
+import org.apache.flink.util.OutputTag;
 
 import java.util.Collections;
 import java.util.Random;
@@ -21,57 +25,35 @@ public class IterationsTest {
     private static final String OUTPUT_FLAG = "o";
 
     public static void main(String[] args) throws Exception {
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironment();
 
-        DataStream<Long> someIntegers = env.addSource(new SourceFunction<Long>() {
-            @Override
-            public void run(SourceContext<Long> sourceContext) throws Exception {
-                sourceContext.collect(3L);
-            }
-
-            @Override
-            public void cancel() {
-
-            }
-        });
-
-        System.out.println(someIntegers.getParallelism());
+        DataStream<Long> someIntegers = env.generateSequence(1, 10);
 
         someIntegers.print();
 
-        IterativeStream<Long> iteration = someIntegers.iterate();
-        System.out.println(iteration.getParallelism());
+        IterativeStream<Long> head = someIntegers.iterate();
 
-        DataStream<Long> minusOne = iteration.map(new MapFunction<Long, Long>() {
+        DataStream<Long> minusOne = head.map(t -> t - 1);
+
+        final OutputTag<Long> outputTag = new OutputTag<Long>("iteration"){};
+
+        SingleOutputStreamOperator<Long> result = minusOne.process(new ProcessFunction<Long, Long>() {
             @Override
-            public Long map(Long value) throws Exception {
-                return value - 1 ;
+            public void processElement(Long value, Context ctx, Collector<Long> out) throws Exception {
+                if (value <= 0){
+                    // 输出到下游
+                    out.collect(value);
+                }else{
+                    // 输出到测游，再返回给迭代头
+                    ctx.output(outputTag, value);
+                }
             }
         });
 
-        System.out.println(minusOne.getParallelism());
+        // 提供给该closeWith函数的DataStream将反馈到迭代头(head), 也就是将大于0的数据返回给迭代头再迭代运行，直到能输出到下游
+        head.closeWith(result.getSideOutput(outputTag));
 
-        DataStream<Long> stillGreaterThanZero = minusOne.filter(new FilterFunction<Long>() {
-            @Override
-            public boolean filter(Long value) throws Exception {
-                return (value > 0);
-            }
-        }).setParallelism(1);
-
-        System.out.println(stillGreaterThanZero.getParallelism());
-
-        iteration.closeWith(stillGreaterThanZero);
-
-//        stillGreaterThanZero.print();
-
-        DataStream<Long> lessThanZero = minusOne.filter(new FilterFunction<Long>() {
-            @Override
-            public boolean filter(Long value) throws Exception {
-                return (value <= 0);
-            }
-        });
-
-        lessThanZero.print();
+        result.print();
 
         env.execute();
 
